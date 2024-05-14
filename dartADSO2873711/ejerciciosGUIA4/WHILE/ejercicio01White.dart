6e23a7b6dfc8fdd844551a8de7a3914fc86fde74
mariapmelosolano <mariapaulamelosolano@gmail.com>
2024-05-13 22:39:58 -0500

    nuevo

---
+++ dartADSO2873711/ejerciciosGUIA4/WHILE/ejercicio01White.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+void main() {
+  //Maria Paula Melo Solano- ejercicio01
+  /*
+    1. Una compañía de seguros tiene contratados a n vendedores. Cada uno hace tres ventas a la semana. 
+    Su política de pagos es que un vendedor recibe un sueldo base, y un 10% extra por comisiones de sus 
+    ventas. El  gerente  de  su  compañía  desea  saber  cuanto  dineroobtendrá  en  la  semana 
+    cada vendedor por concepto de comisiones por las tres ventas realizadas, y cuanto tomando
+    en cuenta su sueldo base y sus comisiones.
+  */
+
+  //DECLARACION 
+  int cantVendedores, contador = 0;
+  double sueldoBase, comisiones =0, porcentajeComision = 0, sueldoTotal, precioVenta;
+
+  //ENTRADA 
+  print ("Ingrese numero de vendedores"); 
+  cantVendedores = int.parse(stdin.readLineSync()!);
+
+  //PROCESO 
+  while ( contador < cantVendedores ) {             
+      print ("Ingrese su sueldo base"); 
+      sueldoBase = double.parse(stdin.readLineSync()!);
+      contador ++;
+
+      for (int i = 0; i < cantVendedores; i++) {     
+        print ("Digite el valor de su venta " +(i+1).toString());
+        precioVenta = double.parse(stdin.readLineSync()!);
+        porcentajeComision = precioVenta * 0.10;
+      }
+
+      comisiones= porcentajeComision * 3;
+      sueldoTotal = sueldoBase + comisiones; 
+      print("Su sueldo base es de: $sueldoBase");
+      print("Y su sueldo mas comisiones es: $sueldoTotal");
+  }
+
+
+
+
+}
